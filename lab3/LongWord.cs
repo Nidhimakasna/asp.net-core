b0ea7567e67c3caaa3bf54c5913ef6731d69ca70
Nidhimakasna <nidhimakasna@gmail.com>
2025-06-17 22:17:50 +0530

    Add files via upload

---
+++ lab3/LongWord.cs
@@ -0,0 +1,25 @@
+﻿
+
+////9.Write a program to find the longest word in a string. 
+class LongWord
+{
+    static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string sen = Console.ReadLine();
+
+        string[] words = sen.Split(' ');
+
+        string longest = words[0];
+
+        foreach (string i in words)
+        {
+            if (i.Length > longest.Length)
+            {
+                longest = i;
+            }
+        }
+
+        Console.WriteLine("Longest word: " + longest);
+    }
+}
