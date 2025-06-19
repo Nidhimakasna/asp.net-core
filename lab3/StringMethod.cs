e95ff21065d6d6dd4d1f389fe6df917fe3fec809
Nidhimakasna <nidhimakasna@gmail.com>
2025-06-19 19:23:41 +0530

    Update StringMethod.cs

--- lab3/StringMethod.cs
+++ lab3/StringMethod.cs
@@ -1,12 +1,19 @@
-﻿////5. Write program showing use of common methods of String class. 
 
-//string text = "Hello World";
-//Console.WriteLine(text.ToUpper());
-//Console.WriteLine(text.ToLower());
+////5. Write program showing use of common methods of String class. 
 
-//string txt1 = "Darshan";
-//string txt2 = "University";
-//Console.WriteLine(String.Concat(txt1 + txt2));
+string text1 = "Hello World";
+string text2 = "Darshan University";
+
+Console.WriteLine("Upper of string:"+text1.ToUpper());
+Console.WriteLine("Lower of string:"+text2.ToLower());
+Console.WriteLine("Trim"+text1.Trim());
+Console.WriteLine( "length of string:"+text2.Length);
+Console.WriteLine("replace the word:"+text1.Replace("World","Everyone"));
+Console.WriteLine("substring:"+text2.Substring(2,5));
+Console.WriteLine("StartsWith:"+text1.StartsWith('n'));
+Console.WriteLine("endswith:"+text2.EndsWith('y'));
+Console.WriteLine("Concat the string"+String.Concat(text1 + text2));
+Console.WriteLine("index of string:"+text2.IndexOf('n'));
 
 
 
