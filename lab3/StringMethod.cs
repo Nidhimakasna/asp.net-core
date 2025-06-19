1be0f65f85ecf30fd66920d07166e66a85de1c57
Nidhimakasna <nidhimakasna@gmail.com>
2025-06-19 19:23:01 +0530

    Add files via upload

---
+++ lab3/StringMethod.cs
@@ -0,0 +1,13 @@
+﻿////5. Write program showing use of common methods of String class. 
+
+//string text = "Hello World";
+//Console.WriteLine(text.ToUpper());
+//Console.WriteLine(text.ToLower());
+
+//string txt1 = "Darshan";
+//string txt2 = "University";
+//Console.WriteLine(String.Concat(txt1 + txt2));
+
+
+
+
