023c02632b86a5df17988b61db9d94ce5a552bc9
Nidhimakasna <nidhimakasna@gmail.com>
2025-06-20 16:01:19 +0530

    Add files via upload

---
+++ lab4/BankAccount.cs
@@ -0,0 +1,105 @@
+﻿////6.Create a BankAccount class having constructor accepting initialBalance and 
+////accountHolderName. Also create Deposite() and withdraw() overloaded 
+////methods by which user can deposit/withdraw amount using different types
+////of parameters(ex.cash, check).
+
+class BankAccount
+{
+    string accountHolderName;
+    double initialBalance;
+    public BankAccount(string name, double balance)
+    {
+        accountHolderName = name;
+        initialBalance = balance;
+    }
+    public void Deposit(double amount)
+    {
+        initialBalance += amount;
+    }
+    public void Deposit(string checkNumber,double amount)
+    {
+        initialBalance += amount;
+    }
+    public void Withdraw(double amount) {
+        if (amount <=  initialBalance)
+        {
+            initialBalance -= amount;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient Balance!");
+        }
+    }
+
+    public void Withdraw(string checkNumber, double amount) {
+        if (amount <= initialBalance)
+        {
+            initialBalance -= amount;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient Balance!");
+        }
+    }
+    public void ShowBalance()
+    {
+        Console.WriteLine("Current Balance: " + initialBalance);
+    }
+}
+//using System;
+
+//class BankAccount
+//{
+//    string accountHolderName;
+//    double balance;
+
+//    // Constructor
+//    public BankAccount(string name, double initialBalance)
+//    {
+//        accountHolderName = name;
+//        balance = initialBalance;
+//    }
+//    public void Deposit(double amount)
+//    {
+//        balance += amount;
+//        Console.WriteLine("Cash Deposited: " + amount);
+//    }
+
+//    public void Deposit(string checkNumber, double amount)
+//    {
+//        balance += amount;
+//        Console.WriteLine("Deposited using Check No. " + checkNumber + ": " + amount);
+//    }
+
+//    public void Withdraw(double amount)
+//    {
+//        if (amount <= balance)
+//        {
+//            balance -= amount;
+//            Console.WriteLine("Cash Withdrawn: " + amount);
+//        }
+//        else
+//        {
+//            Console.WriteLine("Insufficient Balance!");
+//        }
+//    }
+
+//    public void Withdraw(string checkNumber, double amount)
+//    {
+//        if (amount <= balance)
+//        {
+//            balance -= amount;
+//            Console.WriteLine("Withdrawn using Check No. " + checkNumber + ": " + amount);
+//        }
+//        else
+//        {
+//            Console.WriteLine("Insufficient Balance!");
+//        }
+//    }
+
+//    public void ShowBalance()
+//    {
+//        Console.WriteLine("Current Balance: " + balance);
+//    }
+//}
+
