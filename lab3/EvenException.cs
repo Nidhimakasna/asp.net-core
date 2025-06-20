17c24fd44da637f80dd26f18f48ca7159ed951f4
Nidhimakasna <nidhimakasna@gmail.com>
2025-06-20 16:25:15 +0530

    Add files via upload

---
+++ lab3/EvenException.cs
@@ -0,0 +1,27 @@
+﻿
+//8.Write a program to accept a number from the user and throw an exception 
+//if the number is not an even number. 
+using System;
+
+class EvenException
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        try
+        {
+            if (n % 2 != 0)
+            {
+                throw new Exception("Number is not even.");
+            }
+
+            Console.WriteLine("The number is even.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+}
